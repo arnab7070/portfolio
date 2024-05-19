a5037fd98898646d9e59c48461c20e03c012e0fe
Arnab Nandi <arnabnandi7074@gmail.com>
2024-05-19 17:38:24 +0530

    updated resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./dist/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'copper': '#a8763eff',
+        'cosmic-latte': '#f7f3e3ff',
+        'antiflash-white': '#ecf0f1ff',
+        'barn-red': '#6f1a07ff',
+        'bistre': '#2b2118ff',
+      },
+    },
   },
   plugins: [],
 }
